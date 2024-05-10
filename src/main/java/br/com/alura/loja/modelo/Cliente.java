f8c73aa23a733b9158e8a30e9121d52f5594d597
armanoalves <armanobarrosalvesh@gmail.com>
2024-05-10 07:20:38 -0300

    Aula 03 - Performance de consultas

--- src/main/java/br/com/alura/loja/modelo/Cliente.java
+++ src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -9,6 +9,22 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "clientes")
 public class Cliente {
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
